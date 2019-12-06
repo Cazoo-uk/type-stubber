4cebb6f66616a7ccdb13c22def4e82fb395ac62b
Dan Cohen <dan.cohen@cazoo.co.uk>
2019-12-06 15:25:30 +0000

    add the typeStubber class

---
+++ typeStubber.ts
@@ -0,0 +1,33 @@
+export class Stubber<T> {
+  private stub: T;
+  private actions: {
+    (stub: T): void;
+  }[];
+
+  public constructor(defaultStub: T) {
+    this.stub = defaultStub;
+    this.actions = [];
+  }
+
+  public set(action: (stub: T) => void): Stubber<T> {
+    this.actions.push(action);
+    return this;
+  }
+
+  public with(stub: Partial<T>) {
+    this.stub = {
+      ...this.stub,
+      ...stub
+    };
+
+    return this;
+  }
+
+  public build(): T {
+    this.actions.forEach(action => {
+      action(this.stub);
+    });
+
+    return this.stub as T;
+  }
+}
